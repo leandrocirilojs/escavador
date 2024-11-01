5d5e320f16c9c24b0e4f04513dc006975e135c26
leandrocirilojs <48395196+leandrocirilojs@users.noreply.github.com>
2024-11-01 00:58:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ document.querySelectorAll('.footer-icon').forEach(icon => {
         icon.classList.add('active');
     });
 });
+
+//abrir zap
+
+function openWhatsApp() {
+    const phoneInput = document.getElementById('phone-input').value;
+    const formattedNumber = phoneInput.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+
+    if (formattedNumber) {
+        const whatsappURL = `https://wa.me/${formattedNumber}`;
+        window.open(whatsappURL, '_blank'); // Abre o WhatsApp em uma nova aba
+    } else {
+        alert("Por favor, digite um número de telefone válido.");
+    }
+}
